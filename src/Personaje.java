8dd7447c08bb2b12c696d2a91856f98a3b4516f9
@Rockerillo <hugo.v.rock@gmail.com>
2026-02-27 20:30:02 +0100

    Clase Personaje

--- src/Personaje.java
+++ src/Personaje.java
@@ -1,8 +1,83 @@
 public class Personaje {
 
-    private int poder;
+    //ATRIBUTOS
+
     private String nombre;
+    private int vida;
     private int fuerza;
+    private int velocidad;
+    private int defensa;
+
+
+
+
+    //CONSTRUCTORES
+
+    public Personaje() {
+        this.nombre = "Lady Bug";
+        this.vida = 100;
+        this.fuerza = 10;
+        this.velocidad = 15;
+        this.defensa = 10;
+
+    }
+
+    public Personaje (String nombre, int vida, int fuerza, int velocidad, int defensa) {
+        this.nombre = "Lady Bug";
+        this.vida = vida;
+        this.fuerza = fuerza;
+        this.velocidad = velocidad;
+        this.defensa = defensa;
+
+    }
+
+
+
+    //GETERS
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getVida() {
+        return this.vida;
+    }
+
+    public int getFuerza() {
+        return this.fuerza;
+    }
+
+    public int getVelocidad () {
+        return this.velocidad;
+    }
+
+    public int getDefensa() {
+        return this.defensa;
+    }
+
+
+    //SETERS
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+
+
+
+    //METODOS VARIOS
 
 
 
