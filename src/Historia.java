b83a0f368452afebb144423a6aba34922dad0b0a
@Rockerillo <hugo.v.rock@gmail.com>
2026-03-02 11:11:23 +0100

    Clase Historia

---
+++ src/Historia.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Historia {
+
+    //Necesitamos un metodo que nos imprima la historia parte por parte y dependiendo de la zona que el
+    //usuario elija saldrá un texto u otro, basado en un "IF".
+
+
+
+
+}
